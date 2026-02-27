70db887e21d0c3ec1d74ae7fb54135ad6149d929
gineza1-hash <g.ineza1@alustudent.com>
2026-02-27 10:48:26 -0800

    Args Commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,13 +4,13 @@ import sys
 if __name__ == "__main__":
     argv = sys.argv[1:]
     count = len(argv)
-    
+
     if count == 0:
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
     else:
         print("{:d} arguments:".format(count))
-        
+
     for i, arg in enumerate(argv, 1):
         print("{:d}: {}".format(i, arg))
